3af34cb221f16eb7d9380ba2732268f44f56147a
Will Kelley <willkelley1202@gmail.com>
2025-11-13 12:40:27 -0600

    change mod layout

--- keyboards/sofle/rev1/keymaps/wk/keymap.c
+++ keyboards/sofle/rev1/keymaps/wk/keymap.c
@@ -27,26 +27,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_NO, KC_1,        KC_2,        KC_3,        KC_4,        KC_5,     KC_6,   KC_7,        KC_8,        KC_9,        KC_0,           KC_NO,
-  KC_NO, KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,      KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC,
+  KC_TAB, KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,      KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC,
   KC_ESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,      KC_H,    LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT), KC_SCLN,
-  KC_NO, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,      KC_MUTE, KC_NO,        KC_N,         KC_M,         KC_COMM,KC_DOT,  KC_SLSH, KC_NO,
-  KC_NO, KC_NO,       KC_TAB ,       LT(_NAV,      KC_SPC),  LT(_MOUSE, KC_ENT),MO(_SYM),      LT(_NUM,      KC_BSPC),        LT(_FUN, KC_DEL), KC_NO, KC_NO
+  KC_NO, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,      KC_MUTE, KC_NO,        KC_N,         KC_M,         KC_COMM,KC_DOT,  KC_SLSH, KC_ENT,
+  KC_NO, KC_NO,       LT(_MOUSE, KC_ENT) ,       LT(_NAV,      KC_SPC),  KC_LSFT,MO(_SYM),      LT(_NUM,      KC_BSPC),        LT(_FUN, KC_DEL), KC_NO, KC_NO
 ),
 
 [_COLEMAK] = LAYOUT(
   KC_NO, KC_1,        KC_2,        KC_3,        KC_4,        KC_5,     KC_6,   KC_7,        KC_8,        KC_9,        KC_0,           KC_NO,
-  KC_NO, KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,      KC_J,    KC_L,         KC_U,         KC_Y,         KC_QUOT,         KC_BSPC,
+  KC_TAB, KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,      KC_J,    KC_L,         KC_U,         KC_Y,         KC_QUOT,         KC_BSPC,
   KC_ESC, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G,      KC_M,    LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O),    KC_SCLN,
-  KC_NO, KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,      KC_MUTE, KC_NO,        KC_K,         KC_H,         KC_COMM,KC_DOT,  KC_SLSH, KC_NO,
-  KC_NO, KC_NO,       KC_TAB ,       LT(_NAV,      KC_SPC),  LT(_MOUSE, KC_ENT),MO(_SYM),      LT(_NUM,      KC_BSPC),        LT(_FUN, KC_DEL), KC_NO, KC_NO
+  KC_NO, KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,      KC_MUTE, KC_NO,        KC_K,         KC_H,         KC_COMM,KC_DOT,  KC_SLSH,KC_ENT ,
+  KC_NO, KC_NO,       LT(_MOUSE, KC_ENT) ,       LT(_NAV,      KC_SPC),  KC_LSFT,MO(_SYM),      LT(_NUM,      KC_BSPC),        LT(_FUN, KC_DEL), KC_NO, KC_NO
 ),
 
 [_ENTHIUM] = LAYOUT(
         KC_NO, KC_1,        KC_2,        KC_3,        KC_4,        KC_5,     KC_6,   KC_7,        KC_8,        KC_9,        KC_0,           KC_NO,
-  KC_NO, KC_Z,         KC_Y,         KC_O,         KC_U,         KC_EQL,      KC_Q,    KC_L,         KC_D,         KC_P,         KC_X,         KC_BSPC,
+  KC_TAB, KC_Z,         KC_Y,         KC_O,         KC_U,         KC_EQL,      KC_Q,    KC_L,         KC_D,         KC_P,         KC_X,         KC_BSPC,
   KC_W, LGUI_T(KC_C), LALT_T(KC_I), LCTL_T(KC_A), LSFT_T(KC_E), KC_SCLN,      KC_K,    LSFT_T(KC_H), LCTL_T(KC_T), LALT_T(KC_N), LGUI_T(KC_S),    KC_F,
-  KC_NO, KC_QUOT,         KC_MINS,         KC_COMM,         KC_DOT,         KC_SLSH,      KC_MUTE, KC_NO,        KC_J,         KC_M,         KC_G,         KC_B,  KC_V, KC_ENT,
-  KC_NO, KC_NO,       KC_ESC ,       LT(_NAV,      KC_SPC),  LT(_MOUSE, KC_TAB),LT(_SYM, KC_BSPC),      LT(_NUM,      KC_R),        LT(_FUN, KC_DEL), KC_NO, KC_NO
+  KC_ESC, KC_QUOT,         KC_MINS,         KC_COMM,         KC_DOT,         KC_SLSH,      KC_MUTE, KC_NO,        KC_J,         KC_M,         KC_G,         KC_B,  KC_V, KC_ENT,
+  KC_NO, KC_NO,       LT(_MOUSE, KC_ESC) ,       LT(_NAV,      KC_SPC),  KC_LSFT,LT(_SYM, KC_BSPC),      LT(_NUM,      KC_R),        LT(_FUN, KC_DEL), KC_NO, KC_NO
     ),
 
 [_NUM] = LAYOUT(
