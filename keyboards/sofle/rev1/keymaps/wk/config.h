fc949e36e394ce6f0dd41dcb519262334fd3224c
Will Kelley <willkelley1202@gmail.com>
2026-03-03 23:50:58 -0600

    clean up

--- keyboards/sofle/rev1/keymaps/wk/config.h
+++ keyboards/sofle/rev1/keymaps/wk/config.h
@@ -5,24 +5,12 @@
 #define CAPS_WORD_INVERT_ON_SHIFT
 
 
-// Mouse key speed and acceleration.
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY          0
-#undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL       16
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
-#undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
-#undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
-
 // home row mods
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
 #define FLOW_TAP_TERM 120
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 120
 
 // auto shift
 #define NO_AUTO_SHIFT_ALPHA
