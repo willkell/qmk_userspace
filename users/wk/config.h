6e58bc95390f6eb8ec71cf906e700015c249d952
Will Kelley <will@localhost.localdomain>
2026-07-06 16:50:00 -0500

    optimize numbers a lil

--- users/wk/config.h
+++ users/wk/config.h
@@ -5,10 +5,10 @@
 #define CAPS_WORD_INVERT_ON_SHIFT
 
 // home row mods
-#define TAPPING_TERM 200
+#define TAPPING_TERM 250
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
-#define FLOW_TAP_TERM 175
+#define FLOW_TAP_TERM 150
 #define QUICK_TAP_TERM 120
 
 #define COMBO_SHOULD_TRIGGER
